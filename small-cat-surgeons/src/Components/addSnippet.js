77516b0765ca5f18ddbd50d57627a0d8eb00e100
kkopp <kristinakopp@Kristinas-MacBook-Pro.local>
2019-03-30 10:57:57 -0600

    add code

---
+++ small-cat-surgeons/src/Components/addSnippet.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class addSnippet extends Component {
+  render() {
+    return (
+      <div>
+          Add Snippet
+      </div>
+    );
+  }
+}
+
+export default addSnippet
---
+++ small-cat-surgeons/src/Components/comment.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class comment extends Component {
+  render() {
+    return (
+      <div>
+          Comment
+      </div>
+    );
+  }
+}
+
+export default comment
