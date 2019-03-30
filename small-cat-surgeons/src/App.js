cf4cbe33b813f6dfc68c7151043b20d51b84fb5c
reidgarner <reidbgarner@gmail.com>
2019-03-30 13:24:03 -0600

    aligning for merge

--- small-cat-surgeons/src/App.js
+++ small-cat-surgeons/src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
-import CodeSnippet from './Components/CodeSnippet.js';
-import Comment from './Components/Comment.js';
+import CodeSnippet from './Components/CodeSnippet';
+import Comment from './Components/Comment';
 
 
 
