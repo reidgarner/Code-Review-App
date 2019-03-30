d8170e6733c76072f9e498d4206f86f0f853b75d
kkopp <kristinakopp@Kristinas-MacBook-Pro.local>
2019-03-30 12:03:19 -0600

    added more front end code

--- small-cat-surgeons/src/App.js
+++ small-cat-surgeons/src/App.js
@@ -6,11 +6,49 @@ import Comment from './Components/Comment.js';
 
 
 
+
+
 class App extends Component {
+
+constructor () {
+  super();
+  this.state ={
+    codeSnippet: '',
+    snippets: null
+  }
+}
+
+compentDidMount(){
+
+}
+
+onSubmitSnippet = (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  let newSnippet = {
+    codeSnippet: formData.get('Code')
+  }
+  return fetch('',{
+    method:'POST',
+    headers:{
+      'Content-Type': "application/json"
+    },
+    body: JSON.stringify(newSnippet)
+  })
+  .then(response => response.json())
+  .then(response => {
+    this.setState({
+      snippets: [...this.state.snippets, response[0]]
+    })
+  })
+}
+
+
+
   render () {
     return (
       <div className="App">
-        <CodeSnippet />
+        <CodeSnippet onSubmitSnippet={this.onSubmitSnippet} />
         <Comment />
       </div>
     );
