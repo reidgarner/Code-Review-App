1104272c98d744098e2cea976a4483938795382c
Tara <tboyle10@gmail.com>
2019-03-30 14:16:39 -0600

    fixed comments

--- small-cat-surgeons/django/codereview/reviewapp/models.py
+++ small-cat-surgeons/django/codereview/reviewapp/models.py
@@ -4,11 +4,12 @@ from django.db import models
 from django.db import models
 
 class Code(models.Model):
-  title_text = models.CharField(max_length=200)
-  code_text = models.TextField()
+	title_text = models.CharField(max_length=200)
+	code_text = models.TextField()
+	
 
-  def __str__(self):
-  	return self.code_text
+	def __str__(self):
+		return self.title_text
 
 class Comments(models.Model):
   title = models.ForeignKey(Code, on_delete=models.CASCADE)
