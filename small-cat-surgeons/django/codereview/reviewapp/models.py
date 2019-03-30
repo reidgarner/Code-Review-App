42c58598dce8acb1d954c72e6f77ce54481dfb4a
Tara <tboyle10@gmail.com>
2019-03-30 12:30:27 -0600

    added admin

--- small-cat-surgeons/django/codereview/reviewapp/models.py
+++ small-cat-surgeons/django/codereview/reviewapp/models.py
@@ -7,7 +7,13 @@ class Code(models.Model):
   title_text = models.CharField(max_length=200)
   code_text = models.TextField()
 
+  def __str__(self):
+  	return self.code_text
+
 class Comments(models.Model):
   title = models.ForeignKey(Code, on_delete=models.CASCADE)
   comment_text = models.TextField()
   votes = models.IntegerField(default=0)
+
+  def __str__(self):
+  	return self.comment_text
